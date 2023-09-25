d15d45f4263571f948b1d63a909a5eab035ac51b
Dmitry Nazarov <d.a.nazarov1@yandex.ru>
2023-09-25 23:10:32 +0300

    Completed all tasks from first homework (JavaScript Basics)

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,19 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (!name) {
+    return false;
+  }
+
+  if (name.includes(" ")) {
+    return false;
+  }
+
+  if (name.length < 4) {
+    return false;
+  }
+
+  return true;
 }
 
 function sayHello() {
