7771501f8b9fe5110a3735e1c0afd0235b118b87
Dmitry Nazarov <d.a.nazarov1@yandex.ru>
2023-10-06 14:00:00 +0300

    Completed all tasks from second homework (JavaScript Basics)

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,8 @@
 function isEmpty(obj) {
-  // ваш код...
+  // Делаем цикл по свойствам объекта, если есть хотя бы одно... в теле цикла произойдет возврат "false"
+  for (prop in obj) {
+    return false;
+  }
+
+  return true;
 }
